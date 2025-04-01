3b297b82278434774b4d2b07c97490bebacb52e8
macocci7 <macocci7@yahoo.co.jp>
2025-04-01 11:50:28 +0900

    Inertia scaffolding

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps({ user: Object });
+</script>
+
+<template>
+  <AppLayout>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+    <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
+  </AppLayout>
+</template>
